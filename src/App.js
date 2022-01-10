d0d8873787d35f4acda85e5059fe6f8cf19f6e2a
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-01-10 16:44:43 +0200

    refactoring App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 import PropTypes from "prop-types";
 import SearchBar from "./components/Searchbar";
@@ -6,75 +6,57 @@ import ImageGallery from "./components/ImageGallery";
 import ButtonLoadMore from "./components/Button";
 import ModalWindow from "./components/Modal";
 
-class App extends React.Component {
-  static propTypes = {
-    query: PropTypes.string,
-    results: PropTypes.array,
-    loadMore: PropTypes.bool,
-    showModal: PropTypes.bool,
-    filteredResults: PropTypes.array,
-    handleFormSubmit: PropTypes.func,
-    handleLoadingUpd: PropTypes.func,
-    handleOnClick: PropTypes.func,
-    toggleModal: PropTypes.func,
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const [loadMore, setLoadMore] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [filteredResults, setFilteredResults] = useState(null);
+
+  const handleFormSubmit = (name) => {
+    setQuery(name);
   };
 
-  state = {
-    query: "",
-    results: [],
-    loadMore: false,
-    showModal: false,
-    filteredResults: null,
+  const handleLoadingUpd = (results) => {
+    setResults(results);
+    setLoadMore(false);
   };
 
-  handleFormSubmit = (name) => {
-    debugger;
-    this.setState({ query: name });
+  const handleOnClick = (loadMore) => {
+    setLoadMore(loadMore);
   };
 
-  handleLoadingUpd = (results) => {
-    this.setState({ results });
-    this.setState({ loadMore: false });
+  const toggleModal = (id) => {
+    setShowModal((showModal) => !showModal);
+
+    setFilteredResults(results.filter((result) => result.id === id)[0]);
   };
 
-  handleOnClick = (loadMore) => {
-    this.setState({ loadMore });
-  };
-
-  toggleModal = (id) => {
-    this.setState((state) => ({
-      showModal: !state.showModal,
-    }));
-
-    this.setState({
-      filteredResults: this.state.results.filter(
-        (result) => result.id === id
-      )[0],
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <SearchBar onSubmit={this.handleFormSubmit} />
-        <ImageGallery
-          query={this.state.query}
-          onUpd={this.handleLoadingUpd}
-          loadMore={this.state.loadMore}
-          onClick={this.toggleModal}
-        />
-        {this.state.showModal && (
-          <ModalWindow
-            filteredResults={this.state.filteredResults}
-            onClose={this.toggleModal}
-          />
-        )}
-        {this.state.results.length > 0 && (
-          <ButtonLoadMore onClick={this.handleOnClick} />
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <SearchBar onSubmit={handleFormSubmit} />
+      <ImageGallery
+        query={query}
+        onUpd={handleLoadingUpd}
+        loadMore={loadMore}
+        onClick={toggleModal}
+      />
+      {showModal && (
+        <ModalWindow filteredResults={filteredResults} onClose={toggleModal} />
+      )}
+      {results.length > 0 && <ButtonLoadMore onClick={handleOnClick} />}
+    </div>
+  );
 }
 
-export default App;
+App.propTypes = {
+  query: PropTypes.string,
+  results: PropTypes.array,
+  loadMore: PropTypes.bool,
+  showModal: PropTypes.bool,
+  filteredResults: PropTypes.array,
+  handleFormSubmit: PropTypes.func,
+  handleLoadingUpd: PropTypes.func,
+  handleOnClick: PropTypes.func,
+  toggleModal: PropTypes.func,
+};
